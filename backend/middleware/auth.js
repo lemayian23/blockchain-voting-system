ec861e5f3afe60d781f4c2db68e26b735f104f1c
lemayian23 <lemayianledavit2018@gmail.com>
2026-08-18 12:12:11 +0300

    JWT Authentication

---
+++ backend/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
+
+function generateToken(studentId, name) {
+    return jwt.sign({ studentId, name }), JWT_SECRET, { expiresIn: '8h' });
+}
+
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(402).json({error: 'No token provided'});
+    }
+    const parts = authHeader.split(' ');
+    if (parts.length !==2 || parts[0] !== 'Bearer') {
+        return res.status(401).json({ error: 'Invalid token format'});
+    }
+    const token = parts[1];
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.student = decoded; // { studentId, name, iat, exp }
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: 'Invalid or expired token'});
+    }
+}
+
+module.exports = { generateToken, verifyToken };
